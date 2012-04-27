b3ffe3c8493491a778dc1039c421d4f0d2807dc9
Jordan Vieira <jordanrvieira@gmail.com>
2012-04-27 13:16:29 -0300

    user.js commit3

--- user/user.js
+++ user/user.js
@@ -1,19 +1,28 @@
-(function (window) {
-	var user = function(ID,email, firstName, lastName, password); 
+(function (window) 
+{
+	var user = function(ID, emailAddress, firstName, lastName, password); 
 	{
-	this.ID=ID;
-	this.email=email;
-	this.firstName=firstName;
-	this.lastName=lastName;
-	this.password=password;
+	  this.initialize(ID);
+	  this.initialize(emailAddress);
+	  this.initialize(firstName);
+	  this.initialize(lastName);
+	  this.initialize(password);
 	}
-	{
 	var p=user.prototype
-	p.ID=ID;
-	p.email="jordan@raisedmedia.com";
-	p.firstName="Jordan";
-	p.lastName="Vieira";
-	p.password="********";
+	  
+	  p.ID="JordanVieira";
+	  p.email="jordan@raisedmedia.com";
+	  p.firstName="Jordan";
+	  p.lastName="Vieira";
+	  p.password="********";
+	
+	p.initialize = function(ID, emailAddress, firstName, lastName, password);
+	{
+		this.ID = ID;
+		this.emailAddress = emailaddress;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
 	}
 	window.user=user;
 })(window);
